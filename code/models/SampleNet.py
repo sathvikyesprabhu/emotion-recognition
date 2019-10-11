b90cbddfd37a7558a491dc9f12f1500a96ba804d
Russel Dsouza <russel.dsouza47@gmail.com>
2019-10-11 07:04:58 +0530

    Commit before review

--- code/models/SampleNet.py
+++ code/models/SampleNet.py
@@ -4,7 +4,11 @@ from torch import nn
 class SampleNet(nn.Module):
     def __init__(self):
         super(SampleNet, self).__init__()
-        pass
+        encoder = nn.Sequential(
+            nn.Linear(5, 5),
+            nn.Linear(5, 13)
+        )
+        classifier = nn.Softmax()
 
     def forward(self, x):
-        pass
+        return self.classifier(self.encoder(x))
